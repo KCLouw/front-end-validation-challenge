3734217b792925a441bb032cd4e6917e987c9315
KCLouw <klouw07@gmail.com>
2023-02-05 17:31:28 +0200

    finished my form validation and it works

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -1,16 +1,11 @@
 function myValidation(){
     let userNewTask = document.getElementById("newTask").value;
-    let listTask = document.createElement("li");
-    let taskText = document.createTextNode(userNewTask);
-    let x = listTask.appendChild(taskText);
-    let y = document.getElementById("newTaskList").appendChild(x);
-    console.log(y);
-    console.log(x);
-    console.log(listTask);
-    console.log(taskText);
     if(userNewTask.includes("<" || ">")){
         document.getElementById("errorMessage").style.display = "block";
     }else{
-        document.getElementById("errorMessage").innerHTML = " ";
-    }
-}
+        let listTask = document.createElement("li");
+        let taskText = document.createTextNode(userNewTask);
+        let x = listTask.appendChild(taskText);
+        let y = document.getElementById("newTaskList").appendChild(listTask);
+    };
+};

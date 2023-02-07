cad79f7dadebe082ef97bbcc7c9d3db67881b2b7
KCLouw <klouw07@gmail.com>
2023-02-07 18:05:43 +0200

    made some minor fixes

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -1,8 +1,9 @@
 function myValidation(){
     let userNewTask = document.getElementById("newTask").value;
-    if(userNewTask.includes("<" || ">")){
+    if(userNewTask.includes("<") || userNewTask.includes(">")){
         document.getElementById("errorMessage").style.display = "block";
     }else{
+        document.getElementById("errorMessage").style.display = "none";
         let listTask = document.createElement("li");
         let taskText = document.createTextNode(userNewTask);
         let x = listTask.appendChild(taskText);
